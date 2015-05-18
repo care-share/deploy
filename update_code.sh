344bbc0e3992082593984b7a41b0349ec35d567c
Samuel Sayer <ssayer@mitre.org>
2015-05-18 15:37:52 -0400

    Submodule Sync

--- update_code.sh
+++ update_code.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git submodule foreach "(echo pwd;git checkout master; git pull; cd ..; git add '$path'; git commit -m 'Submodule Sync')"
+git submodule foreach "(echo $pwd;git checkout master; git pull; cd ..; git add '$path'; git commit -m 'Submodule Sync')"
